7f88a8d9b6aa57377b166e10e2015b3691099423
marksuurland <marksuurland1@gmail.com>
2015-09-04 11:52:20 +0200

    finished assigement 1

--- Mark/Week 1/opdracht1.hs
+++ Mark/Week 1/opdracht1.hs
@@ -12,7 +12,6 @@ fromDigits = foldl addDigit 0
    where addDigit num d = 10*num + d
 		 
 -- Exercise 1 tests ---
-
 testLastDigit :: (Integer, Integer) -> Bool
 testLastDigit (n, d) = lastDigit n == d
 
@@ -29,7 +28,26 @@ testToRevDigits (n,d) = toRevDigits n == d
 
 -- Exercise 3 -- 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther (x:xs) = 2 * x : doubleEveryOther xs 
+doubleEveryOther = zipWith (*) (cycle [1,2])
 
-evenIndex :: [Integer] -> [Integer]
-evenIndex (x:xs) = if even then x : evenIndex xs else evenIndex xs 
+--Exercise 3 Tests---
+testDoubleEveryOther :: ([Integer], [Integer]) -> Bool
+testDoubleEveryOther (n,d) = doubleEveryOther n == d
+
+--Exercise 4--
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = sum (digits x) + sumDigits xs  
+
+--Exercise 4 Tests--- 
+testSumDigits:: ([Integer], Integer) -> Bool
+testSumDigits (n,d) = (sumDigits n) == d
+
+--Exercise 5--
+luhn :: Integer -> Bool
+luhn x = (sumDigits (doubleEveryOther (toRevDigits x))) `mod` 10 == 0
+
+--Exercise 5 Tests--
+testLuhn:: (Integer, Bool) -> Bool
+testLuhn (n,d) = (luhn n) == d
+
